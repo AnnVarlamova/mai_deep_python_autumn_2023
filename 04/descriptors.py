7e7021b6d72610ab549a27d66b8935916b6139af
AnnVarlamova <74834132+AnnVarlamova@users.noreply.github.com>
2023-11-03 00:38:08 +0300

    Update descriptors.py

--- 04/descriptors.py
+++ 04/descriptors.py
@@ -1,3 +1,10 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+# Makes dollars from rub
+
+
 class RubToUsdConverter:
     def __init__(self, exchange_rate):
         self.exchange_rate = exchange_rate
@@ -9,6 +16,7 @@ class RubToUsdConverter:
         instance.rub_amount = value * self.exchange_rate
 
 
+# Favorite dollar rate
 class Price:
     def __init__(self, rub_amount):
         self.rub_amount = rub_amount
@@ -16,6 +24,8 @@ class Price:
     usd = RubToUsdConverter(70)
 
 
+# speed control. Doesn't allow you to increase the
+# border further, doesn't allow you to reverse it
 class SpeedLimitDescriptor:
     def __init__(self, max_speed):
         self.max_speed = max_speed
@@ -32,6 +42,7 @@ class SpeedLimitDescriptor:
             instance._speed = value
 
 
+# The car increases and decreases speed
 class Car:
     speed = SpeedLimitDescriptor(max_speed=120)
 
@@ -45,6 +56,7 @@ class Car:
         self.speed -= amount
 
 
+# Does not believe in the existence of negative calories
 class CalorieDescriptor:
     def __get__(self, instance, owner):
         return instance._calories
@@ -55,6 +67,7 @@ class CalorieDescriptor:
         instance._calories = value
 
 
+# Simply records calories, you can count them if you wish
 class Food:
     calories = CalorieDescriptor()
 
